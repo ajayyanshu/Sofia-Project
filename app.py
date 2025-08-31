72e6ff93cdf33a2f691e7285fcb89f142090b0cd
Ajay kumar <108951992+ajayyanshu@users.noreply.github.com>
2025-09-01 02:32:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,11 @@
 import os
 from flask import Flask, render_template
-from dotenv import load_dotenv
-
-# Load environment variables from .env file
-load_dotenv()
 
 app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
 def home():
-    gemini_api_key = os.getenv('GOOGLE_API_KEY')
-    if not gemini_api_key:
-        return "Internal Server Error: Gemini API key is not set. Please set the GOOGLE_API_KEY environment variable.", 500
+    gemini_api_key = "AIzaSyDSVYwHKLSd_R4HOKDTW8dCY1eY9TvbnP4"
     return render_template('recreated_ui.html', gemini_api_key=gemini_api_key)
 
 if __name__ == '__main__':
