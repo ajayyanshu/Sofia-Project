b1fe94f3e84339c6685196d84443e9f0ad175b9f
Ajay kumar <108951992+ajayyanshu@users.noreply.github.com>
2025-09-03 21:50:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__, template_folder='templates')
 @app.route('/')
 def home():
     gemini_api_key = "AIzaSyDSVYwHKLSd_R4HOKDTW8dCY1eY9TvbnP4"
-    return render_template('recreated_ui.html', gemini_api_key=gemini_api_key)
+    return render_template('index.html', gemini_api_key=gemini_api_key)
 
 if __name__ == '__main__':
     app.run(debug=True)
