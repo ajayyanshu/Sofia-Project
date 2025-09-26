4d68ba9598511ebd6deb0689fa3ab2db94f17fc6
Ajay kumar <108951992+ajayyanshu@users.noreply.github.com>
2025-09-27 00:39:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -67,7 +67,7 @@ PDF_KEYWORDS = {
 
 @app.route('/')
 def home():
-    return render_template('comming_soon.html')
+    return render_template('coming_soon.html')
 
 
 # --- Helper Functions for File Processing ---
